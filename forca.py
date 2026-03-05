1dce66f9df50f5bf2f345758c2ab0148eff85977
ISAGOME <gomes.pereira.isabela@escola.pr.gov.br>
2026-03-05 14:18:00 -0300

    forca.py

--- forca.py
+++ forca.py
@@ -3,7 +3,12 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+   palavra_secreta = "banana"
+
+while(não acertou E não enforcou):
+    print("Jogando...")
+
     print("Fim do jogo")
-    
+
     if(__name__ == "__main__"):
     jogar()
