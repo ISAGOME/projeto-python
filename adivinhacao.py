14ca9257bbce83edc9c3e1234cab640065ef636e
ISAGOME <gomes.pereira.isabela@escola.pr.gov.br>
2026-03-15 21:01:12 -0300

    adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,36 @@
+import random
 
+def jogar():
+    
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    # restante do código omitido
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+if (__name__ == "__main__"):
+    jogar()
